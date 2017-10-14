0a8e5391f0596a8c02d34d61079a0dcfb8e8ec2c
Embo <kiki452totoro@gmail.com>
2017-10-14 23:24:51 +0000

    Fixing page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,11 +6,12 @@ class Movie < ActiveRecord::Base
 class Movie::InvalidKeyError < StandardError ; end
   
   def self.find_in_tmdb(string)
-   begin require 'themoviedb'
-       TMdb::Api.key("f4702b08c0ac6ea5b51425788bb262562")
-     Tmdb::Movie.find(string)
-     rescue Tmdb::InvalidApiKeyError
-        raise Movie::InvalidKeyError, 'Invalid API key'
+    begin 
+      require 'themoviedb'
+      TMdb::Api.key("f4702b08c0ac6ea5b51425788bb262562")
+      Tmdb::Movie.find(string)
+    rescue Tmdb::InvalidApiKeyError
+      raise Movie::InvalidKeyError, 'Invalid API key'
     end
   end
 
